803ef9361e88c5e32624813d6cd44ca60cfa8cba
ilkeryuce1 <109898238+ilkeryuce1@users.noreply.github.com>
2022-10-27 19:18:10 +0300

    veritabanında gün ve saat ayrıldı doktor ismine göre randevular listelendi Doktor Panelinde bugunki randevular listelendi

--- HastaneBilgiYonetimSistemi/Doktor.Designer.cs
+++ HastaneBilgiYonetimSistemi/Doktor.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -41,6 +44,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Randevular";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -60,16 +64,39 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(299, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(457, 196);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(645, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 4;
+            // 
             // Doktor1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Doktor1";
+            this.Text = "Doktor Ekranı";
             this.Load += new System.EventHandler(this.Doktor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +107,7 @@
         public System.Windows.Forms.Button button1;
         public System.Windows.Forms.Button button2;
         public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.Label label2;
     }
 }
--- HastaneBilgiYonetimSistemi/Doktor.cs
+++ HastaneBilgiYonetimSistemi/Doktor.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,15 +25,39 @@ namespace HastaneBilgiYonetimSistemi
 
         private void Doktor_Load(object sender, EventArgs e)
         {
+            label2.Text = DateTime.Now.ToString("yyyy-MM-dd"); //bugunse bugunku randevular cıkması ıcın 
             //select P_Ad+' '+P_Soyad as 'Ad Soyad' from tbl_Personel where Personel_id=1
             SqlCommand komut = new SqlCommand($"select P_Ad+' '+P_Soyad as 'Ad Soyad' from tbl_Personel where Personel_id={kul_id}", bgl.bagla());
             DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(komut);
+            SqlDataAdapter da = new SqlDataAdapter(komut);           
             da.Fill(dt);
-            if (dt.Rows.Count > 0)
+            SqlDataReader oku = komut.ExecuteReader();
             {
-                MessageBox.Show("asda");
+                if (oku.Read())
+                {
+                    label1.Text = (oku["Ad Soyad"]).ToString();
+                    
+                }
             }
+            bgl.bagla().Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            goster();
+        }
+        private void goster()
+        {
+            SqlCommand sqlData = new SqlCommand($"select h.Tc,h.Ad + ' ' + h.Soyad as 'Ad Soyad',h.Cinsiyet,r.R_Gun,r.R_Saat from tbl_Randevular r inner join tbl_Hasta h on r.Hasta_id=h.Hasta_id inner join tbl_Personel p on p.Personel_id=r.Doktor_id where p.Personel_id=1 and R_Gun like'%{label2.Text.ToString()}%'", bgl.bagla());
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter sd = new SqlDataAdapter(sqlData);
+            sd.Fill(dataTable);
+            sqlData.ExecuteNonQuery();
+            dataGridView1.DataSource = dataTable;
+
+           bgl.bagla().Close();
 
         }
     }
