3e1ac6bfa45fe165c4e81ad2319deb2637071a05
ilkeryuce1 <109898238+ilkeryuce1@users.noreply.github.com>
2022-10-28 12:28:37 +0300

    tüm hastaları oruntuleme ve asta recete guncelleme ekleme uncellendı

--- HastaneBilgiYonetimSistemi/Doktor.Designer.cs
+++ HastaneBilgiYonetimSistemi/Doktor.Designer.cs
@@ -28,32 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.reçeteYazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tahlilİsteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 171);
+            this.button1.Location = new System.Drawing.Point(12, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 67);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Randevular";
+            this.button1.Text = "Bugünki Rabdevular";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 270);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 67);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Reçete";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -66,37 +69,133 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(299, 171);
+            this.dataGridView1.Location = new System.Drawing.Point(282, 91);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(457, 196);
+            this.dataGridView1.Size = new System.Drawing.Size(692, 496);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.Visible = false;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(645, 38);
+            this.label2.Location = new System.Drawing.Point(868, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 16);
             this.label2.TabIndex = 4;
+            this.label2.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(725, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reçeteYazToolStripMenuItem,
+            this.tahlilİsteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(150, 52);
+            // 
+            // reçeteYazToolStripMenuItem
+            // 
+            this.reçeteYazToolStripMenuItem.Name = "reçeteYazToolStripMenuItem";
+            this.reçeteYazToolStripMenuItem.Size = new System.Drawing.Size(149, 24);
+            this.reçeteYazToolStripMenuItem.Text = "Reçete Yaz";
+            // 
+            // tahlilİsteToolStripMenuItem
+            // 
+            this.tahlilİsteToolStripMenuItem.Name = "tahlilİsteToolStripMenuItem";
+            this.tahlilİsteToolStripMenuItem.Size = new System.Drawing.Size(149, 24);
+            this.tahlilİsteToolStripMenuItem.Text = "Tahlil İste";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(498, 198);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 67);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 67);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Tanı ve istem ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(195, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 67);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(282, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 351);
+            this.panel1.TabIndex = 11;
+            this.panel1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 67);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Tüm Randevular";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Doktor1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(987, 642);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Doktor1";
             this.Text = "Doktor Ekranı";
             this.Load += new System.EventHandler(this.Doktor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,9 +204,18 @@
         #endregion
 
         public System.Windows.Forms.Button button1;
-        public System.Windows.Forms.Button button2;
         public System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem reçeteYazToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tahlilİsteToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button3;
+        public System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Button button5;
     }
 }
